9721cbb1d91905cc74973726a59308e40828b955
Muhammed Mustafa <muhammed.mustafa.helal@gmail.com>
2022-02-10 21:22:45 +0100

    created the initial layout of the application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
 
-import { Switch, Route, Link } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom';
+import { Switch } from 'react-router'
 import { Layout, Typography, Space } from 'antd'
+
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, CryptoDetails, News } from './components/components';
+import './App.css';
 
 const App = () => {
   return (
-    <div>Hello World</div>
+    <div className='app'>
+      <div className='navbar'>
+        <Navbar />
+      </div>
+      <div className='main'>
+        <Layout>
+          <div className='routes'>
+            <Switch>
+              <Route exact path='/'>
+                <Homepage />
+              </Route>
+              <Route exact path='/exchanges'>
+                <Exchanges />
+              </Route>
+              <Route exact path='/cryptocurrencies'>
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path='/crypto/:coinId'>
+                <CryptoDetails />
+              </Route>
+              <Route exact path='/news'>
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+        <div className='footer' >
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+            Cryptoverse<br />
+            All rights reserved
+          </Typography.Title>
+          <Space >
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
+    </div>
   )
 }
 
