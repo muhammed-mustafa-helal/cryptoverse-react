b582c4f713407650df0f605077efa5cf5e712bff
Muhammed Mustafa <muhammed.mustafa.helal@gmail.com>
2022-02-10 17:44:41 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import { Switch, Route, Link } from 'react-router-dom'
+import { Layout, Typography, Space } from 'antd'
+
+const App = () => {
+  return (
+    <div>Hello World</div>
+  )
+}
+
+export default App
