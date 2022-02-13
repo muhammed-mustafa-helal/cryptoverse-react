6dd830465a2ccf825e5f2fd7e8c80dbf1f8ae0fb
Muhammed Mustafa <muhammed.mustafa.helal@gmail.com>
2022-02-13 12:15:13 +0100

    Fetched bing news about cryptocurrencies news and implemented the component to display them

--- src/services/cryptoAPI.js
+++ src/services/cryptoAPI.js
@@ -4,8 +4,9 @@ const cryptoApiHeaders = {
     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
     'x-rapidapi-key': '57dfc5c35cmsh5e60e57c80445adp1b6c1ajsn5b47f10014e4'
 };
-const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
 export const cryptoApi = createApi({
     reducerPath: 'cryptoApi',
